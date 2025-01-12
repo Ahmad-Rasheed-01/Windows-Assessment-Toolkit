8c9948d25df6c2d3f5f0fa5f2c9b4fb5d74e7aa6
Ahmad Rasheed <ahmadrasheed5130@gmail.com>
2025-01-12 23:39:57 +0500

    Added inline intro.

---
+++ code/wat.ps1
@@ -0,0 +1,22 @@
+#powershell
+
+"""Windows Assessment Toolkit"""
+
+#############################################################################
+##                                                                         ##
+## DESCRIPTION: Windows toolkit for workstation and server written         ##
+## in PowerShell to assess security.                                       ##
+##                                                                         ##
+## FILENAME: wat.ps1                                                       ##
+## VERSION: 0.01                                                           ##
+## STATUS: DEV                                                             ##
+## LAST MOD: 1/12/25 @ 1:36 PM                                             ##
+## AUTHOR: Ahmad Rasheed                                                   ##
+#############################################################################
+
+<#
+Notes:
+
+#>
+
+
---
+++ code/wat_workstation.ps1
@@ -0,0 +1,23 @@
+#powershell
+
+"""Windows Assessment Toolkit"""
+
+#############################################################################
+##                                                                         ##
+## DESCRIPTION: Windows toolkit for workstation and server written         ##
+## in PowerShell to assess security.                                       ##
+##                                                                         ##
+## FILENAME: wat-workstation.ps1                                           ##
+## VERSION: 0.01                                                           ##
+## STATUS: DEV                                                             ##
+## LAST MOD: 1/12/25 @ 1:36 PM                                             ##
+## AUTHOR: Ahmad Rasheed                                                   ##
+## Supported Version: Windows 10, 11                                       ##
+#############################################################################
+
+<#
+Notes:
+
+#>
+
+
